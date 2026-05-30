499c6625d65a759185278a8310d5e577a54bebae
amitpaswan202 <paswanamit512@gmail.com>
2026-05-30 11:07:52 +0530

    cors error solve deployment 1 setup

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,10 @@ const productRoutes = require("./src/routes/product.routes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+  }));
+
 app.use(express.json());
 app.use("/api/products", productRoutes);
 
--- frontend/src/services/productService.js
+++ frontend/src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/products";
+const API_URL = "https://mern-industrial-app.onrender.com/api/products";
 
 export const getProducts = async () => {
   const response = await axios.get(API_URL);
