cc5ce40159f26214d9a583d28fd5242e2e16ee8e
amitpaswan202 <paswanamit512@gmail.com>
2026-06-02 13:11:12 +0530

    image slider in  change in production deployment 1 setup

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
-
+import SimpleImageSlider from "react-simple-image-slider";
+import img1 from "../images/1.jpg";
+import img2 from "../images/2.jpg";
+import img3 from "../images/3.jpg";
 function About() {
+  const images = [
+   { url: img1 },
+   { url :  img2},
+   { url :  img3}
+
+  ];
+
   return (
-    <div>About</div>
+    <>
+     <div>About
+
+    </div>
+   <SimpleImageSlider
+    width={window.innerWidth}
+        height={500}
+        images={images}
+        showBullets={true}
+        showNavs={true}
+        autoPlay={true}
+         autoPlayDelay={3}
+   />
+    </>
+   
   )
 }
 
